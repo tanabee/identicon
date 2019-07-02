93c8bee4b326286074b82a2dcb82a3269932c10f
tanabee <tanabee1986@gmail.com>
2019-07-02 16:46:59 +0900

    Handle POST request

--- app/identicon.py
+++ app/identicon.py
@@ -1,24 +1,32 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 import requests
+import hashlib
 
 app = Flask(__name__)
+salt = "UNIQUE_SALT"
 default_name = 'tanabee'
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def mainpage():
     
     name = default_name
 
+    if request.method == 'POST':
+        name = request.form['name']
+
+    salted_name = salt + name
+    name_hash = hashlib.sha256(salted_name.encode()).hexdigest()
+
     header = '<html><head><title>Identicon</title></head><body>'
     body = '''
         <form method="POST">
-        Hello <input type="text" name="name" value="{}">
+        Hello <input type="text" name="name" value="{0}">
         <input type="submit" value="submit">
         </form>
         <p>You look like a:
-        <img src="/monster/monster.png">
+        <img src="/monster/{1}" />
         </p>
-    '''.format(name)
+    '''.format(name, name_hash)
     footer = '</body></html>'
     return header + body + footer
 
