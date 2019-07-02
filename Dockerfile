cb2664bb3eacf6aea24efe92c2b25eef5b5b0f33
tanabee <tanabee1986@gmail.com>
2019-07-02 20:16:01 +0900

    Add cache using Redis

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3.7
 
 RUN groupadd -r uwsgi && useradd -r -g uwsgi uwsgi
-RUN pip install Flask==1.0.2 uWSGI==2.0.18 requests==2.22.0
+RUN pip install Flask==1.0.2 uWSGI==2.0.18 requests==2.22.0 redis==2.10.3
 WORKDIR /app
 COPY app /app
 COPY cmd.sh /
--- app/identicon.py
+++ app/identicon.py
@@ -1,8 +1,10 @@
 from flask import Flask, Response, request
 import requests
 import hashlib
+import redis
 
 app = Flask(__name__)
+cache = redis.StrictRedis(host='redis', port=6379, db=0)
 salt = "UNIQUE_SALT"
 default_name = 'tanabee'
 
@@ -32,8 +34,12 @@ def mainpage():
 
 @app.route('/monster/<name>')
 def get_identicon(name):
-    r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
-    image = r.content
+    image = cache.get(name)
+    if image is None:
+        print("Cache miss", flush=True)
+        r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
+        image = r.content
+        cache.set(name, image)
 
     return Response(image, mimetype='image/png')
 
